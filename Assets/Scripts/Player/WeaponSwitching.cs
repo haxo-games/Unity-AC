3e280da7cf608dbd144d35b1bd52e226629a7866
Luna <lunatattersall@gmail.com>
2025-07-15 22:50:04 +0100

    Weapon Switching ready to test

--- Assets/Scripts/Player/WeaponSwitching.cs
+++ Assets/Scripts/Player/WeaponSwitching.cs
@@ -25,11 +25,42 @@ public class WeaponSwitching : MonoBehaviour
     }
     private void SetWeapons()
     {
-        throw new NotImplementedException();
+        weapons = new Transform[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            weapons[i] = transform.GetChild(i);
+        }
+
+        if (keys == null) keys = new KeyCode[weapons.Length];
     }
 
-    private void Select(int selectedWeapon)
+    private void Update()
     {
-        throw new NotImplementedException();
+        int previousSelectedWeapon = selectedWeapon;
+
+        for (int i = 0; i < keys.Length; i++)
+        {
+            if (Input.GetKeyDown(keys[i]) && timeSinceLastSwitch >= switchTime)
+                selectedWeapon = i;
+        }
+        if (previousSelectedWeapon != selectedWeapon) Select(selectedWeapon);
+
+        timeSinceLastSwitch += Time.deltaTime;
+    }
+
+    private void Select(int WeaponIndex)
+    {
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            weapons[i].gameObject.SetActive(i == WeaponIndex);
+        }
+        timeSinceLastSwitch = 0f;
+
+        OnWeaponSelected();
+    }
+    private void OnWeaponSelected()
+    {
+        print("New weapon Selected...");
     }
 }
